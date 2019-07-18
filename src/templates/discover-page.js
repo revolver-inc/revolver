64f80ba8f4cfe42c751726a850ad499698509d15
David Bergeron <davidbergeron@Davids-MacBook-Pro.local>
2019-07-17 19:55:05 -0600

    Round 2. Trying to fix missing page issue

---
+++ src/templates/discover-page.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+// import BlogRoll from "../components/BlogRoll"
+
+const IndexPageTemplate = ({ data }) => {
+  const { markdownRemark } = data
+  const { html, frontmatter } = markdownRemark
+  const { title, heading, subheading, description, featuredImage } = frontmatter
+
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`Game Creator's Space`, `GCS`, `NAIT`]} />
+      <section className="home-intro">
+        <Image name="record-store.png" />
+        {/* <h1>{title}</h1> */}
+        <div className="intro-content">
+          <h3>{heading}</h3>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+        </div>
+      </section>
+      {/* <div
+        className="main-content"
+        dangerouslySetInnerHTML={{ __html: html }}
+      /> */}
+    </Layout>
+  )
+}
+
+export default IndexPageTemplate
+
+export const pageQuery = graphql`
+  query IndexPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      html
+      frontmatter {
+        title
+        heading
+      }
+    }
+  }
+`
