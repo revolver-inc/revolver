51f6934201aade3ccab5d9efb966385085db258e
David Bergeron <davidbergeron@Davids-MacBook-Pro.local>
2019-07-17 22:14:51 -0600

    reworked config for better list manipulation

---
+++ src/pages/products/MUSIC-ArcadeFire-Funeral.md
@@ -0,0 +1,15 @@
+---
+templateKey: product-page
+productType: music
+title: MUSIC-ArcadeFire-Funeral
+name: Arcade Fire
+subtitle: Funeral
+genre: Alternative
+productImg: /assets/arcade-fire.jpg
+isLocal: false
+condition: Vinyl (Mint)
+mediaUrl: "https://www.youtube.com/watch?v=3nq5UHA8Ag4"
+id: 47b0e871-3068-4b55-8627-d20e70de8759
+---
+
+Sounds. Excitement. This album is worthy of bombastic adjectives, and racous celebration. If you listen to this album, you will hear magic.
---
+++ src/templates/product-page.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+// import BlogRoll from "../components/BlogRoll"
+
+const ProductPageTemplate = ({ data }) => {
+  const { markdownRemark } = data
+  const { html, frontmatter } = markdownRemark
+  const { title, heading, subheading, description, featuredImage } = frontmatter
+
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`Game Creator's Space`, `GCS`, `NAIT`]} />
+      <section className="home-intro">
+        <Image name="record-store.png" />
+        {/* <h1>{title}</h1> */}
+        <div className="intro-content">
+          <h3>{heading}</h3>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+        </div>
+      </section>
+      {/* <div
+        className="main-content"
+        dangerouslySetInnerHTML={{ __html: html }}
+      /> */}
+    </Layout>
+  )
+}
+
+export default ProductPageTemplate
+
+export const pageQuery = graphql`
+  query ProductPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      html
+      frontmatter {
+        title
+        heading
+      }
+    }
+  }
+`
