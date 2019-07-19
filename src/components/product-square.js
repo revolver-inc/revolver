b57259cf5e2e70d32529e0f30bfa82f6ba7aa94c
David Bergeron <david.f.bergeron@gmail.com>
2019-07-18 18:58:36 -0600

    Sliders are properly linked to product pages

--- src/components/product-square.js
+++ src/components/product-square.js
@@ -1,15 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
 import Image from "./image"
 
 const ProductSquare = ({ product }) => {
   const img = product.productImg.relativePath
   return (
     <div className="product-square">
-      <div>
-        <Image name={img} />
-      </div>
-      <h4>{product.title}</h4>
-      <h5>{product.subtitle}</h5>
+      <Link to={`/products/${product.name}`}>
+        <div>
+          <Image name={img} />
+        </div>
+        <h4>{product.title}</h4>
+        <h5>{product.subtitle}</h5>
+      </Link>
     </div>
   )
 }
