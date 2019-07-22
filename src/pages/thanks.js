cb68d3a619eb273b3edeef243510462a5db3c0ef
David Bergeron <david.f.bergeron@gmail.com>
2019-07-21 22:21:16 -0600

    Added utility .scss, removed ability to delete pages

---
+++ src/pages/thanks.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ThanksPage = () => (
+  <Layout>
+    <SEO title="Revolver - Thanks!" />
+    <h1>Thanks for contacting us!</h1>
+    <p>We will be in touch with you shortly. </p>
+  </Layout>
+)
+
+export default ThanksPage
